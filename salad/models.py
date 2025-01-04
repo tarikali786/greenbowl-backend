7a29c18a49cd1a9295515c6b91f42e99e714eedd
Tariq Ali <tarikali7444@gmail.com>
2025-01-04 18:21:51 +0530

    add meta db table

--- salad/models.py
+++ salad/models.py
@@ -9,6 +9,9 @@ class Ingridient(models.Model):
     weight = models.CharField(max_length=250, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to="media")
-     
 
+    class Meta:
+        db_table = 'salad_ingredient'
 
+    def __str__(self):
+        return self.name
