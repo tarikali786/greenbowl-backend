1e09f614a9849cad2cf148de3cd7744188e8b0fc
Tariq Ali <tarikali7444@gmail.com>
2025-01-04 17:23:33 +0530

    dj-database-url

--- backend/settings.py
+++ backend/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': dj_database_url.config(default='postgresql://greenbowl_user:qFtXDyt5p6elk1of2NHnRW8SH1hjJXYR@dpg-ctshjllumphs73fmmd60-a.oregon-postgres.render.com/greenbowl')
