df976e90aa1e3f0fba303b84000cd8dce3389f19
Tariq Ali <tarikali7444@gmail.com>
2025-01-04 14:01:52 +0530

    initialize project

--- backend/settings.py
+++ backend/settings.py
@@ -133,10 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = "media/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 MEDIA_ROOT = BASE_DIR / "media"
 
 CORS_ALLOW_ALL_ORIGINS = True
