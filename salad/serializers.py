90f78d95178932361ea02577a57dc3fad8e98b25
Tariq Ali <tarikali7444@gmail.com>
2025-02-12 10:32:38 +0530

    implemented successfuly payment gatway

--- salad/serializers.py
+++ salad/serializers.py
@@ -2,4 +2,7 @@ from rest_framework import serializers
 
 class PaymentIntentSerializer(serializers.Serializer):
     amount = serializers.IntegerField(required=True)
-    
+    email = serializers.EmailField(required=True)
+    phone = serializers.CharField(required=True)
+    name = serializers.CharField(required=True)
+    address = serializers.DictField(child=serializers.CharField(), required=True)
--- salad/views.py
+++ salad/views.py
@@ -1,15 +1,11 @@
 import stripe
 from django.conf import settings
 from rest_framework.response import Response
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.views import APIView, status
+from rest_framework.views import APIView
+from rest_framework import status
 from salad.serializers import PaymentIntentSerializer
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
-
-# Create your views here.
-
-
 
 class CreatePaymentIntent(APIView):
     def post(self, request):
@@ -19,10 +15,11 @@ class CreatePaymentIntent(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
         validated_data = serializer.validated_data
+        
         try:
             payment_intent = stripe.PaymentIntent.create(
                 amount=validated_data['amount'],
-                currency= "INR",
+                currency="INR",
                 payment_method_types=['card'],
                 description="Order Salad",
                 receipt_email=validated_data['email'],
@@ -34,11 +31,11 @@ class CreatePaymentIntent(APIView):
                 shipping={
                     "name": validated_data['name'],
                     "address": {
-                        "line1": validated_data['address'].get('line1', ''),
-                        "city": validated_data['address'].get('city', ''),
-                        "state": validated_data['address'].get('state', ''),
-                        "postal_code": validated_data['address'].get('postal_code', ''),
-                        "country": validated_data['address'].get('country', ''),
+                        "line1": validated_data['address']['line1'],
+                        "city": validated_data['address']['city'],
+                        "state": validated_data['address']['state'],
+                        "postal_code": validated_data['address']['postal_code'],
+                        "country": validated_data['address']['country'],
                     },
                 }
             )
